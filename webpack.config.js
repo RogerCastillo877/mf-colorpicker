7972e0652997693f3f268d4b2b9deb768982d2cf
RogerCastillo877 <roger.castillo877@gmail.com>
2023-09-28 23:41:16 -0500

    dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,66 +2,68 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
-module.exports = (_, argv) => ({
-  output: {
-    publicPath: "https://rocas-color-picker.netlify.app/",
-  },
+module.exports = (_, argv) => {
+  return ({
+    output: {
+      publicPath: argv.mode === "development" ? "http://localhost:3002" : "https://rocas-color-picker.netlify.app/",
+    },
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
 
-  devServer: {
-    port: 3001,
-    historyApiFallback: true,
-  },
+    devServer: {
+      port: 3001,
+      historyApiFallback: true,
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
+          },
         },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
         },
-      },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "mf_colorpicker",
+        filename: "remoteEntry.js",
+        remotes: {},
+        exposes: {
+          "./ColorPicker": "./src/components/ColorPicker.jsx",
+          "./useColors": "./src/hooks/useColors.js"
+        },
+        shared: {
+          ...deps,
+          react: {
+            singleton: true,
+            requiredVersion: deps.react,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: deps["react-dom"],
+          },
+        },
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+      }),
     ],
-  },
-
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "mf_colorpicker",
-      filename: "remoteEntry.js",
-      remotes: {},
-      exposes: {
-        "./ColorPicker": "./src/components/ColorPicker.jsx",
-        "./useColors": "./src/hooks/useColors.js"
-      },
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-});
+  })
+};
